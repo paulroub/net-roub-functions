3530656b4bfd0f9edb7d6e7da43054c3b09142cc
Paul Roub <paul@roub.net>
2020-02-16 14:56:58 -0500

    Actually lookup metadata in head

--- functions/_modules/digger.js
+++ functions/_modules/digger.js
@@ -1,5 +1,19 @@
-function lookupMetadata(url) {
-    return '';
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+function lookupMetadata(html) {
+    const dom = new JSDOM(html);
+    const head = dom.window.document.head;
+
+    const meta = head.querySelector('meta[property="og:title"]');
+
+    if (meta) {
+        return {
+            title: meta.getAttribute('content')
+        };
+    }
+
+    return {};
 }
 
 exports.metadata = lookupMetadata;
--- functions/_test/facebook.test.js
+++ functions/_test/facebook.test.js
@@ -1,5 +1,10 @@
 const digger = require('../_modules/digger');
 
 test('returns empty metadata for an empty head', () => {
-    expect(digger.metadata('')).toEqual('');
+    expect(digger.metadata('')).toEqual({});
+});
+
+test('returns facebook title', () => {
+    const html = `<meta property="og:title" content="Test-driven Development: CodeMash" />`;
+    expect(digger.metadata(html).title).toEqual('Test-driven Development: CodeMash');
 });
