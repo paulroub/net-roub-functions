41c7f1c6594309bbf75c54e32410e4ffd5b129f1
Paul Roub <paul@roub.net>
2020-02-16 15:54:17 -0500

    Don't try to return image without metadata

--- functions/_test/w3c.test.js
+++ functions/_test/w3c.test.js
@@ -9,8 +9,3 @@ test('returns page description', () => {
     const html = `<html><head><meta name="description" content="this is my description" /></head></html>`;
     expect(digger.metadata(html).description).toEqual('this is my description');
 });
-
-test('returns first image on page', () => {
-    const html = `<html><body><img src=http://example.com/foo2.png /></body></html>`;
-    expect(digger.metadata(html).image).toEqual('http://example.com/foo2.png');
-});
