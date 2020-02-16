ac9ffeb3be68ea96bb73ea79620633217ddebc06
Paul Roub <paul@roub.net>
2020-02-16 15:39:35 -0500

    Test that we get facebook images

--- functions/_test/facebook.test.js
+++ functions/_test/facebook.test.js
@@ -13,3 +13,8 @@ test('returns facebook description', () => {
     const html = `<html><head><meta property="og:description" content="this is my description" /></head></html>`;
     expect(digger.metadata(html).description).toEqual('this is my description');
 });
+
+test('returns facebook image', () => {
+    const html = `<html><head><meta property="og:image" content="http://example.com/foo.png" /></head></html>`;
+    expect(digger.metadata(html).image).toEqual('http://example.com/foo.png');
+});
